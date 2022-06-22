16eabaa51df87822c4b24a6dcb1bc52541b2eebc
Roy Friedman <royfriedman@Roys-MacBook-Pro.local>
2022-06-22 11:52:50 -0400

    cleanup and documentation

--- EPLL/EPLL_utils.py
+++ EPLL/EPLL_utils.py
@@ -1,37 +1,42 @@
 import numpy as np
 import torch as tp
-from typing import Union, Callable, Tuple
+from typing import Union, Callable, Tuple, List
 from models import Denoiser
-from torchvision import transforms
 import warnings
 tp.set_grad_enabled(False)
 
 _tensor = tp.Tensor
 
 
-def _default_sched(noise_var: float): return lambda i: (1 if i==0 else 2**(i+1))/noise_var
+def _default_sched(noise_var: float) -> Callable: return lambda i: (1 if i==0 else 2**(i+1))/noise_var
 
 
-def _callable_beta(beta: Union[int, float, Callable]):
+def _callable_beta(beta: Union[int, float, Callable]) -> Callable:
+    """
+    Makes sure that beta is a callable function, even if the input was an int or a float
+    :param beta: the value for beta; if an int or float, then a callable that always returns the same value is returned
+    :return: a callable function that returns the value of beta at each iteration
+    """
     if not callable(beta):
         num = beta
         return lambda i: num
     return beta
 
 
-def _choose_grids(p_sz: int, n_grids: int):
-    # choose random, different, grids
-    # chs = np.random.choice(p_sz**2-1, n_grids-2, replace=False)
-    # chs = chs + 1
-    # x0, y0 = chs//p_sz, chs%p_sz
-    # return [int(a) for a in x0] + [0, p_sz-1], [int(a) for a in y0] + [0, p_sz-1]
+def _choose_grids(p_sz: int, n_grids: int) -> Tuple[List, List]:
+    """
+    Helper function to sample the placement of the grids
+    :param p_sz: the sizes of the patches used
+    :param n_grids: the number of grids that should be chosen
+    :return: two lists, the first for the xs and the second for the ys of the upper left corner of each grid
+    """
     chs = np.random.choice(p_sz**2, n_grids, replace=False)
     chs = chs
     x0, y0 = chs//p_sz, chs%p_sz
     return [int(a) for a in x0], [int(a) for a in y0]
 
 
-def pad_im(im: _tensor, p_sz: int, mode: str='reflect', end_values=0.):
+def pad_im(im: _tensor, p_sz: int, mode: str='reflect', end_values=0.) -> _tensor:
     """
     Reflection pad image with given pad size
     :param im: the image to pad; a torch tensor with shape [N, M] or [N, M, 3]
@@ -55,7 +60,7 @@ def pad_im(im: _tensor, p_sz: int, mode: str='reflect', end_values=0.):
                                     mode=mode)).float().to(im.device)
 
 
-def trim_im(im: _tensor, p_sz: int): return im[p_sz//2:-p_sz//2][:, p_sz//2:-p_sz//2]
+def trim_im(im: _tensor, p_sz: int) -> _tensor: return im[p_sz//2:-p_sz//2][:, p_sz//2:-p_sz//2]
 
 
 def to_patches(im: _tensor, p_sz: int, x0: int, y0: int) -> Tuple[_tensor, Tuple]:
@@ -74,9 +79,6 @@ def to_patches(im: _tensor, p_sz: int, x0: int, y0: int) -> Tuple[_tensor, Tuple
     for i in range(outp.shape[0]):
         for j in range(outp.shape[1]):
             outp[i, j] = im[x0+i*p_sz:x0+(i+1)*p_sz, y0+j*p_sz:y0+(j+1)*p_sz]
-    # outp = im.clone()[x0:x1, y0:y1]
-    # outp = outp.unfold(1, p_sz, p_sz).unfold(0, p_sz, p_sz)
-    # outp = outp.permute((0, 1, -1, -2, 2)) if clr else outp.permute((0, 1, -1, -2))
     return outp.reshape(-1, *outp.shape[2:]), outp.shape
 
 
@@ -90,23 +92,16 @@ def to_image(im: _tensor, ps: _tensor, shape: tuple, x0: int, y0: int) -> _tenso
     :param y0: an int depicting the y position of the top left corner of the grid
     :return: the reassembled image as a torch tensor with the same shape as im
     """
-    clr = im.ndim == 3
     outp = im.clone()
     ps = ps.reshape(shape)
     p_sz = ps.shape[2]
     for i in range(ps.shape[0]):
         for j in range(ps.shape[1]):
             outp[x0+i*p_sz:x0+(i+1)*p_sz, y0+j*p_sz:y0+(j+1)*p_sz] = ps[i, j]
-    # ret = ps.reshape(shape)
-    # ret = ret.permute((0, 1, -1, -2, 2)) if clr else ret.permute((0, 1, -1, -2))
-    # ret = ps.reshape(shape[0]*shape[2], shape[1]*shape[3], 3) \
-    #     if clr else ps.reshape(shape[0]*shape[2], shape[1]*shape[3])
-    # outp = im.clone()
-    # outp[x0:x0+ret.shape[0], y0:y0+ret.shape[1]] = ret
     return outp
 
 
-def grid_denoise(im: _tensor, var: float, x0: int, y0: int, denoiser: Denoiser, p_sz: int):
+def grid_denoise(im: _tensor, var: float, x0: int, y0: int, denoiser: Denoiser, p_sz: int) -> _tensor:
     """
     Denoise a grid corrupted by isotropic noise
     :param im: the image to denoise; a torch tensor with shape [N, M] or [N, M, 3]
@@ -118,9 +113,6 @@ def grid_denoise(im: _tensor, var: float, x0: int, y0: int, denoiser: Denoiser, 
                      such that the signature is denoiser(patches, noise_variance)
     :return: the cleaned image as a torch tensor with the same shape as the input image
     """
-    # resize image if needed
-    orig_shp = [im.shape[0], im.shape[1]]
-
     # break to patches
     ps, shp = to_patches(im, p_sz, x0, y0)
 
@@ -134,7 +126,18 @@ def grid_denoise(im: _tensor, var: float, x0: int, y0: int, denoiser: Denoiser, 
 
 
 def optimize_function(loss_func: Callable, params: _tensor, its: int, optimizer: str='adam', lr: float=1e-2,
-                      momentum: float=.9):
+                      momentum: float=.9) -> _tensor:
+    """
+    Helper function for optimizations
+    :param loss_func: a callable loss function that returns a differentiable torch tensor
+    :param params: the parameters that should be optimized, as a torch tensor, which the loss relies on
+    :param its: number of iterations to run the optimization process
+    :param optimizer: a string representing the optimizer that should be used; the known optimizers are: 'sgd',
+                      'adam', 'rmsprop' - if an unkown optimizer is given, will default to 'adam'
+    :param lr: the learning rate that the optimzer should use
+    :param momentum: the momentum the optimizer should use
+    :return: the optimized parameters as a torch tensor
+    """
     if optimizer.lower()=='adam': opt = tp.optim.Adam([params], lr=lr)
     elif optimizer.lower()=='sgd': opt = tp.optim.SGD([params], lr=lr, momentum=momentum)
     elif optimizer.lower()=='rmsprop': opt = tp.optim.RMSprop([params], lr=lr, momentum=momentum)
